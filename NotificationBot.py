5a838a656614648d338a3b9a11e090bdc018ea8e
Orlando <orlandoortegajr@gmail.com>
2019-08-18 00:25:02 -0400

    now able to properly execute script at 9:00 am everyday

--- NotificationBot.py
+++ NotificationBot.py
@@ -1,16 +1,28 @@
 from weather.weather import Weather
-from nba.league import League
 from setup import create_message, subject
 from mail.gmail import send_email
+import schedule
+import time
+
 
 if __name__ == "__main__":
-    #get the desired location's weather data
-    w_data = Weather("Toronto", "Canada")
-    nba_team_name = "Toronto Raptors"
+    def task():
 
-    #create the message text
-    msg_text = create_message(w_data, nba_team_name)
+        #get the desired location's weather data
+        w_data = Weather("Toronto", "Canada")
+        nba_team_name = "Toronto Raptors"
 
-    #send the msg
-    send_email(subject(),msg_text)
+        #create the message text
+        msg_text = create_message(w_data, nba_team_name)
+
+        #send the msg
+        send_email(subject(),msg_text)
+
+    schedule.every().day.at("09:00").do(task)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+    
+
 
